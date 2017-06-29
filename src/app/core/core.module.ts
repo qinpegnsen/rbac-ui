5c86792ac037578b27e7bac2f2d9a6cdb3ed2a4b
likun <koupengxiang@163.com>
2017-06-29 18:37:56 +0800

    add 添加正则常量  修改完善

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,28 +1,25 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
-
-import { SettingsService } from './settings/settings.service';
-import { ThemesService } from './themes/themes.service';
-import { TranslatorService } from './translator/translator.service';
-import { MenuService } from './menu/menu.service';
-
-import { throwIfAlreadyLoaded } from './module-import-guard';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import {SettingsService} from './settings/settings.service';
+import {ThemesService} from './themes/themes.service';
+import {TranslatorService} from './translator/translator.service';
+import {MenuService} from './menu/menu.service';
+import {throwIfAlreadyLoaded} from './module-import-guard';
+import {PatternService} from './forms/pattern.service';
 
 @NgModule({
-    imports: [
-    ],
-    providers: [
-        SettingsService,
-        ThemesService,
-        TranslatorService,
-        MenuService
-    ],
-    declarations: [
-    ],
-    exports: [
-    ]
+  imports: [],
+  providers: [
+    SettingsService,
+    ThemesService,
+    TranslatorService,
+    MenuService,
+    PatternService
+  ],
+  declarations: [],
+  exports: []
 })
 export class CoreModule {
-    constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
-        throwIfAlreadyLoaded(parentModule, 'CoreModule');
-    }
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
 }
