6840774e0f8962dbdc13d2e35fa7b7e3e9129b5f
borong <zdfwlxfz_dd@163.com>
2017-06-29 09:28:58 +0800

    init 项目

---
+++ src/app/routes/menu.ts
@@ -0,0 +1,16 @@
+
+const Home = {
+    text: 'Home',
+    link: '/home',
+    icon: 'icon-home'
+};
+
+const headingMain = {
+    text: '主菜单',
+    heading: true
+};
+
+export const menu = [
+    headingMain,
+    Home
+];
