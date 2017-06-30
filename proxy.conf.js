e49dde39edae9e2c285eb8cdc7d60166332a88da
shangze <nsh95958@163.com>
2017-06-30 15:14:12 +0800

    修改webpack的proxy配置，解决跨域问题

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,9 +1,17 @@
 const PROXY_CONFIG = [
   {
     context: [
-      "/"
+      "/login",
+      "/staff"
     ],
     target: "http://192.168.10.119:8081",
+    secure: false
+  },
+  {
+    context: [
+      "/upload"
+    ],
+    target: "http://192.168.10.119:8083",
     secure: false
   }
 ];
