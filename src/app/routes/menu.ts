e91b11c85a0a1c10958a26645eff03a565db0f77
borong <zdfwlxfz_dd@163.com>
2017-07-04 09:42:45 +0800

    up 菜单权限

--- src/app/routes/menu.ts
+++ src/app/routes/menu.ts
@@ -1,27 +1,91 @@
-const Home = {
-  text: 'Home',
-  link: '/home',
-  icon: 'icon-home'
-};
-
-const headingMain = {
+const SystemMain = {
   text: '主菜单',
   heading: true
 };
-const datatables = {
-  text: 'datatables',
-  link: '/datatables',
-  icon: 'icon-speedometer'
-};
-const msg = {
-  text: '弹框和消息通知',
-  link: '/msg',
-  icon: 'icon-volume-2'
+
+/**
+ * 系统管理
+ * @type {{text: string; link: string; icon: string; submenu: [{text: string; link: string},{text: string; link: string}]}}
+ */
+const system = {
+  text: '系统管理',
+  icon: 'fa-2x fa fa-gears text-center',
+  alert: 'child',
+  submenu: [
+    {
+      text: '平台系统',
+      link: '/system/v1'
+    },
+    {
+      text: '系统属性设置',
+      link: '/system/v2'
+    },
+    {
+      text: '密码修改',
+      link: '/system/v2'
+    }
+  ]
 };
 
+/**
+ * 机构管理
+ * @type {{text: string; link: string; icon: string}}
+ */
+const organ = {
+  text: '机构管理',
+  link: '/organ',
+  icon: 'fa-2x fa fa-institution text-center'
+};
+
+/**
+ * 权限管理
+ * @type {{text: string; link: string; icon: string}}
+ */
+const limit = {
+  text: '权限管理',
+  link: '/limit',
+  icon: 'fa-2x fa fa-lock text-center'
+};
+
+/**
+ * 角色管理
+ * @type {{text: string; link: string; icon: string}}
+ */
+const role = {
+  text: '角色管理',
+  link: '/role',
+  icon: 'fa-2x fa fa-group text-center'
+};
+
+/**
+ * 账户管理
+ * @type {{text: string; link: string; icon: string}}
+ */
+const account = {
+  text: '账户管理',
+  icon: 'fa-2x fa fa-user text-center',
+  elink: 'http://www.baidu.com/',
+  alert: 'child',
+  submenu: [
+    {
+      text: '管理员管理',
+      link: '/account/v1'
+    },
+    {
+      text: '员工账户管理',
+      link: '/account/v2'
+    }
+  ]
+};
+
+/**
+ * 菜单配置
+ * @type {[{text: string; link: string; icon: string; submenu: {text: string; link: string}[]},{text: string; link: string; icon: string},{text: string; link: string; icon: string},{text: string; link: string; icon: string},{text: string; link: string; icon: string; submenu: {text: string; link: string}[]}]}
+ */
 export const menu = [
-  headingMain,
-  Home,
-  datatables,
-  msg
+  system,
+  organ,
+  limit,
+  role,
+  account
 ];
