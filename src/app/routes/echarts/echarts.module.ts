397732b3844b8c2e349377d734650713b4f6ac98
likun <koupengxiang@163.com>
2017-07-05 18:55:31 +0800

    up 完善图表插件

--- src/app/routes/echarts/echarts.module.ts
+++ src/app/routes/echarts/echarts.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EchartsComponent } from './echarts/echarts.component';
 import {RouterModule, Routes} from '@angular/router';
-import {Ng2Echarts} from 'ng2-echarts'; //加载图表模块
+import {AngularEchartsModule} from 'ngx-echarts';
 
 const routes: Routes = [
   {path: '', component: EchartsComponent},
@@ -10,9 +10,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
+    AngularEchartsModule,
     RouterModule.forChild(routes),
     CommonModule
   ],
-  declarations: [EchartsComponent,Ng2Echarts] //引入图表模块
+  declarations: [EchartsComponent] //引入图表模块
 })
 export class EchartsModule { }
