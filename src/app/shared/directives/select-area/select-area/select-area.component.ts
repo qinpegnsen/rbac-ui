92c5347c2efedd8e5905b4a3e0cb871c1f2c7951
xiejiaojiao <2583206346@qq.com>
2017-08-05 14:09:31 +0800

    up 区域选择组件

--- src/app/shared/directives/select-area/select-area/select-area.component.ts
+++ src/app/shared/directives/select-area/select-area/select-area.component.ts
@@ -1,7 +1,7 @@
-
-import {Component,EventEmitter, Input, Output, OnInit} from '@angular/core';
-import {RzhtoolsService} from "../../../../core/services/rzhtools.service";
+import {Component, EventEmitter, Input, Output, OnInit} from '@angular/core';
 import {isNullOrUndefined} from 'util';
+import {RzhtoolsService} from "../../../../core/services/rzhtools.service";
+declare var $: any;
 
 @Component({
   selector: 'app-select-area',
@@ -14,55 +14,71 @@ export class SelectAreaComponent implements OnInit {
   private areas: any;
   private areaCode: string = '';
   private adr: string = '';
-
-  @Input() private required:boolean;
-
+  @Input() private required: boolean;
   @Output() myData = new EventEmitter();
 
-
-  constructor(private tools: RzhtoolsService) { }
+  constructor(private tools: RzhtoolsService) {
+  };
 
   ngOnInit() {
-    //console.log("█ required ►►►", this.required);
+    /**
+     * 点击区域选框外页面时，关闭选框
+     * @type {SelectAreaComponent}
+     * @private
+     */
+    let _this = this;
+    $('body').click(function (e) {
+      let event = e.target.attributes['class'];
+      if (isNullOrUndefined(event) || isNullOrUndefined(event.nodeValue) || event.nodeValue.indexOf("rzh-sel-city") <= 0) _this.show = false; //关闭选框
+    });
   }
 
-
-  // 获取地区列表
-  getArea(fullName,myAreaCode,isOld){
+  /**
+   * 获取地区列表
+   * @param fullName 全名
+   * @param myAreaCode 区域编码
+   * @param isOld 是否为老版
+   */
+  getArea(fullName, myAreaCode, isOld) {
     let me = this;
     me.show = true;
-    me.areas = me.tools.getAreaByCode(myAreaCode,isOld).children;
+    me.areas = me.tools.getAreaByCode(myAreaCode, isOld).children;
     me.adr = fullName;
     me.areaCode = myAreaCode;
-    if (isNullOrUndefined(me.areas) || me.areas.length == 0){
+    if (isNullOrUndefined(me.areas) || me.areas.length == 0) {
       me.cityConfirm();
     }
-    //console.log(me.tools.getAreaByCode(myAreaCode,isOld))
   }
 
-  //显示城市选择器并获取省级列表
-  showSelectArea(){
+  /**
+   * 显示城市选择器并获取省级列表
+   */
+  showSelectArea() {
     let me = this;
-    if(me.show) return;
+    if (me.show) return;
     me.show = true;
     me.areas = me.tools.getAreaByCode('');
   }
 
-  //重置城市信息
-  freshCitys(){
+  /**
+   * 重置城市信息
+   */
+  freshCitys() {
     this.adr = '';
     this.areas = this.tools.getAreaByCode('');
   }
 
-  //确定选择城市
-  cityConfirm(){
+  /**
+   * 确定选择城市
+   */
+  cityConfirm() {
     this.show = false;
-    if(this.adr == ''){
+    if (this.adr == '') {
       this.areaCode = ''
     }
     this.myData.emit({
-      areaCode:this.areaCode,
-      adr:this.adr
+      areaCode: this.areaCode,
+      adr: this.adr
     });
   }
 }
