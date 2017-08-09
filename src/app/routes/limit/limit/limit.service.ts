bc674eed500011e504878ed509ac6b48c4882aec
yanweiyi <m15901019392_1@163.com>
2017-08-09 11:18:08 +0800

    up limit权限

--- src/app/routes/limit/limit/limit.service.ts
+++ src/app/routes/limit/limit/limit.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {AjaxService} from "../../../core/services/ajax.service";
+import {isNull, isNullOrUndefined} from "util";
 import {Page} from "../../../core/page/page";
 
 @Injectable()
@@ -8,21 +9,27 @@ export class LimitService {
   constructor(private ajax:AjaxService) {
   }
 
+
   /**
    * 系统列表
-   * @returns {any}
-   */
-  sysList() {
-    let sysList = [];
+   * @param _this 组件信息
+   * @param async true：异步，flase：同步
+   * @returns {Array}
+     */
+  sysList(_this?, async?) {
+    let sysList = [],me = this;
+    if (isNullOrUndefined(async)) async = false;
     this.ajax.get({
       url: '/sys/list',
-      async: false,
+      async: async,
       data: {
         'sysName': ''
       },
       success: (data) => {
         sysList = data;
-        console.log("█ sysList ►►►", sysList);
+        //若传入参数不为空，进行动态赋值
+        if (!isNullOrUndefined(_this)) _this.sysList = data, _this.sysCode = _this.sysList[0].sysCode; //设置系统列表
+        if(async) _this.data = new Page(me.queryMenuList(1, 3, _this.sysCode)); //设置第一个系统列表下的权限信息
       },
       error: (data) => {
         console.log("error");
