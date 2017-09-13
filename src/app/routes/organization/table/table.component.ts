08a2b76d69665db6d774d656fa0206e70dc13f45
yanweiyi <m15901019392_1@163.com>
2017-09-13 09:18:32 +0800

    优化权限系统

--- src/app/routes/organization/table/table.component.ts
+++ src/app/routes/organization/table/table.component.ts
@@ -3,12 +3,14 @@ import {AppStore} from "../store/app-store";
 import {Store} from "@ngrx/store";
 import {AjaxService} from "../../../core/services/ajax.service";
 import {OrgService} from "../server/org.service";
+import {TableService} from "./table.service";
 
 const swal = require('sweetalert');
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
+  providers:[TableService],
 })
 export class TableComponent implements OnInit {
   check_status = []; // 处理全选 不选 等的状态数组
@@ -36,7 +38,8 @@ export class TableComponent implements OnInit {
   constructor(
     private ajax:AjaxService,
     private store: Store<AppStore>,
-    private org:OrgService
+    private org:OrgService,
+    private TableService:TableService
   ) {
     this.buttonConfig = [
       {
@@ -108,7 +111,7 @@ export class TableComponent implements OnInit {
   /**
    * 删除员工
    */
-  delstaffs(){
+  /*delstaffs(){
     this.ajax.post({
       url: '/staff/updateState',
       data: {
@@ -121,16 +124,51 @@ export class TableComponent implements OnInit {
             this.store.dispatch({type: 'STAFF_ADD', payload: res});
           })
         });
-          swal('删除成功', '', 'success');
-
       },
       error: (data) => {
         swal('删除失败提醒', '', 'error');
       }
     });
+  }*/
+
+
+  /**
+   * 删除员工信息
+   * @param delCodeId
+   */
+  delstaffs(delCodeId) {
+    let _this = this, url: string = "/staff/updateState", data: any;
+    swal({
+        title: '确认删除此信息？',
+        type: 'info',
+        confirmButtonText: '确认', //‘确认’按钮命名
+        showCancelButton: true, //显示‘取消’按钮
+        cancelButtonText: '取消', //‘取消’按钮命名
+        closeOnConfirm: false  //点击‘确认’后，执行另外一个提示框
+      },
+      function () {  //点击‘确认’时执行
+        console.log("█ 1 ►►►",  1);
+
+        swal.close(); //关闭弹框
+        data = {
+          'staffCode':_this.srrStaff,
+          'state': 'DEL'
+        }
+        console.log(data)
+        _this.TableService.delCode(url, data); //删除数据
+        _this.store.select('addStaff').subscribe((res) => {
+          _this.org.getOrgList('/staff/list?deptCode=' + res[0].deptCode).subscribe((res) => {
+            _this.store.dispatch({type: 'STAFF_ADD', payload: res});
+          })
+        });
+      }
+    );
   }
 
-
+  /**
+   * 为部门设置主管
+   * @param data
+   */
   upMenudateState(data) {
     console.log("█ data ►►►", data );
 
